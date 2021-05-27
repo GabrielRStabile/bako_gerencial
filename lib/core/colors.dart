e4037f46884e2f0a07e63ebb70fafa612beb2a95
GabrielRStabile <gabrielrostabile@outlook.com>
2021-05-27 17:21:22 -0300

    :lipstick: Adicionado assets e cores

---
+++ lib/core/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0XFFEFC100);
+const secundaryColor = Color(0xFFEED364);
+const darkColor = Color(0xFFB69404);
+const labelColor = Color(0xFFD0D0D0);
+const headerColor = Color(0xFF000000);
+const backgroundColor = Color(0xFFF3F4F8);
