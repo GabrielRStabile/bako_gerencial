cb630ee30b255483debd450d1bd2da7186c31e6c
GabrielRStabile <gabrielrostabile@outlook.com>
2021-06-04 09:51:13 -0300

    :lipstick: Novas cores e atualização de assets
    - Adicionado cores para o InformationCard.
    - Atualizado logotipo e pattern para versão mais colorida e contrastada.

--- lib/core/colors.dart
+++ lib/core/colors.dart
@@ -6,3 +6,6 @@ const darkColor = Color(0xFFB69404);
 const labelColor = Color(0xFFD0D0D0);
 const headerColor = Color(0xFF000000);
 const backgroundColor = Color(0xFFF3F4F8);
+const informationColor = Color(0xFF63C0DF);
+const errorColor = Color(0xFFE45555);
+const successColor = Color(0xFF009944);
