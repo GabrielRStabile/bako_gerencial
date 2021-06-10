f8cfd64c0fb101935aa8305cdad6bebede586c90
GabrielRStabile <gabrielrostabile@outlook.com>
2021-06-10 19:10:09 -0300

    :lipstick: Pattern & Color update
    - For do the background pattern of LoginScreen, I had to change the asset for a png file, because flutter_svg dont accept the pattern tag of svg code.
    
    - Added the inputTextColor thats gonna be used in TextFormField's style.

--- lib/core/colors.dart
+++ lib/core/colors.dart
@@ -4,6 +4,7 @@ const primaryColor = Color(0XFFEFC100);
 const secundaryColor = Color(0xFFEED364);
 const darkColor = Color(0xFFB69404);
 const labelColor = Color(0xFFD0D0D0);
+const inputTextColor = Color(0xFF343434);
 const headerColor = Color(0xFF000000);
 const backgroundColor = Color(0xFFF3F4F8);
 const informationColor = Color(0xFF63C0DF);
